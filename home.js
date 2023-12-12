150548d183d7cd434c1a139536e4f41499eeb56a
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-12-11 17:10:33 -0800

    Create home.js

---
+++ home.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Old Imgflip homepage
+// @namespace   N/A
+// @version     1.0
+// @description Redirects imgflip.com to imgflip.com/m/fun
+// @author      Kokonico
+// @match       https://imgflip.com/
+// @grant       none
+// ==/UserScript==
+
+(function() {
+   'use strict';
+
+   window.location.href = "https://imgflip.com/m/fun";
+})();
